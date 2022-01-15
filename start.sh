8965162e7427e5e341517f0d3c3d6ecc946ea306
phjung1 <fordivil@knou.ac.kr>
2022-01-16 04:32:32 +0900

    test

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 export FLASK_APP=microblog.py
-export FLASK_RUN_PORT=5000
+export FLASK_RUN_PORT=8080
 python3 -m flask run -h 0.0.0.0
