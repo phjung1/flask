95e3b338a52e0773e86f84e718f5e77eb8f71a80
phjung1 <fordivil@knou.ac.kr>
2022-01-17 09:35:09 +0900

    test

---
+++ app/func/test.py
@@ -0,0 +1,4 @@
+import numpy
+
+img = numpy.zeros((5,3),dtype=numpy.uint8)
+print(img.shape)
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+#iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 export FLASK_APP=microblog.py
 export FLASK_RUN_PORT=8080
-python3 -m flask run -h 0.0.0.0
+#nohup python3 -m flask run -h 0.0.0.0 &
+python3 -m flask run -h 0.0.0.0 
